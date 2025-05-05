a2e3ce04e7da7fe31230edfa0b057c0812c1e2f5
Kristiana <k.trupja@reply.de>
2025-05-05 10:18:10 +0200

    update authentication

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,8 +3,10 @@ import { Suspense } from "react";
 import AdminClient from "./AdminClient";
 
 export default function AdminPage() {
+ 
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      
       <AdminClient />
     </Suspense>
   );
